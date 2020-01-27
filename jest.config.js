ccd99dd88681103dbf43abf7082384354238c9e5
tms1337 <faruk1337@gmail.com>
2020-01-27 12:41:04 +0100

    Configure jest and enzyme

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ const jestConfig = {
   globals: {
     $ENV: "test"
   },
+  setupFilesAfterEnv: ["<rootDir>/src/config/testing.js"],
+  snapshotSerializers: ["enzyme-to-json/serializer"]
 };
 
 module.exports = jestConfig;
